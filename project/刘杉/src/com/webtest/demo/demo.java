0d4896313d9b4c135f194bab24ebe46dae500c9b
liushannayulun <409680375@qq.com>
2020-12-06 12:01:05 +0800

    add

---
+++ project/刘杉/src/com/webtest/demo/demo.java
@@ -0,0 +1,14 @@
+package com.webtest.demo;
+
+import static org.testng.Assert.assertTrue;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.webtest.core.BaseTest;
+
+public class demo {
+	@Test()
+	public void demo() {
+		System.out.println("hello");
+	}
+
+}
