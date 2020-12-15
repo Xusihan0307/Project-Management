e669e4310fb7608ee9f82aa246e1e4119e62bcfb
Xusihan0307 <45161005+Xusihan0307@users.noreply.github.com>
2020-12-15 11:13:15 +0800

    121
    121212

---
+++ Demo1.java
@@ -0,0 +1,12 @@
+package com.webtest.demo;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test
+	public void test1() {
+		assertEquals(1, 1);
+	}
+}
